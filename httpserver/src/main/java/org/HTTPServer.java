a5ce995b2774f908878c5d786bf1668c250e2d3b
peengtao123 <peengtao123@126.com>
2019-03-25 16:04:47 +0800

    init

---
+++ httpserver/src/main/java/org/HTTPServer.java
@@ -0,0 +1,44 @@
+package org;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URLDecoder;
+
+public class HTTPServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(8080);
+
+            while (true) {
+                Socket socket = ss.accept();
+                BufferedReader bd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                /**
+                 * 接受HTTP请求
+                 */
+                String requestHeader;
+                while ((requestHeader = bd.readLine()) != null && !requestHeader.isEmpty()) {
+                    System.out.println(URLDecoder.decode(requestHeader));
+                }
+
+                //发送回执
+                PrintWriter pw = new PrintWriter(socket.getOutputStream());
+
+                pw.println("HTTP/1.1 200 OK");
+                pw.println("Content-type:text/html;charset=utf-8");
+                pw.println();
+                pw.println("<h1>success!查</h1>");
+
+                pw.flush();
+                socket.close();
+                System.out.println("----------------------------------------------------------------");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
